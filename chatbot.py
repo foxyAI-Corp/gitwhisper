a69cd5a9b565c145e26fa3833269e299601a3730
foxypiratecove37350 <108185011+foxypiratecove37350@users.noreply.github.com>
2024-08-04 20:43:05 +0200

    Added the argument parsing

--- chatbot.py
+++ chatbot.py
@@ -1,12 +1,21 @@
 import gitwhisper_ai
+from argparse import ArgumentParser
 
-gitwhisper_ai.open_repository(input("Repository path: "))
-gitwhisper_ai.start_chat()
-while True:
-    usr_inp = input("User: ")
-    print("GitWhisper: ", end='')
-    
-    resp = gitwhisper_ai.send_message(usr_inp, stream=True)
+arg_parser = ArgumentParser(description="GitWhisper: A chatbot to help you with Git repositories")
+arg_group = arg_parser.add_mutually_exclusive_group()
+arg_group.add_argument("--ui", action="store_true", default=True, help="Use GitWhiper in a UI")
+arg_group.add_argument("--cli", action="store_true", default=False, help="Use GitWhiper in the terminal")
+args = arg_parser.parse_args()
 
-    for chunk in resp:
-        print(chunk.text, end='')
+if args.cli:
+    gitwhisper_ai.open_repository(input("Repository path: "))
+    gitwhisper_ai.start_chat()
+
+    while True:
+        usr_inp = input("User: ")
+        print("GitWhisper: ", end='')
+
+        resp = gitwhisper_ai.send_message(usr_inp, stream=True)
+
+        for chunk in resp:
+            print(chunk.text, end='')
